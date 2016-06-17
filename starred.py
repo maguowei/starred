6585743160c9cb10eb3ca1ad2baecfd9c7a86300
maguowei <imaguowei@gmail.com>
2016-06-17 10:47:47 +0800

    add link star fork language status

--- starred.py
+++ starred.py
@@ -1,9 +1,24 @@
 from github3 import GitHub
 
 
+desc = '''# starred
+
+## backup
+
+```
+python starred.py > README.md
+
+```
+
+## Repositories
+
+'''
+
 if __name__ == '__main__':
     gh = GitHub()
     stars = gh.iter_starred('maguowei')
+    print(desc)
     for s in stars:
-        print(s)
+        #print('*', '[{}]({}) stars: {}, forks: {}, languages: {}, desc: {}'.format(s.full_name, s.html_url, s.stargazers, s.fork_count, s.language, s.description))
+        print('*', '[{}]({}) stars: {}, forks: {}, languages: {}'.format(s.full_name, s.html_url, s.stargazers, s.fork_count, s.language))
 
