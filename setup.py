827677a6c7ec2ade2e9af7cbc387242d45d25bfd
maguowei <imaguowei@gmail.com>
2016-06-19 20:30:49 +0800

    add command line interfaces

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='starred',
+    version='0.1.3',
+    url='https://github.com/maguowei/starred',
+    license='The MIT License (MIT)',
+    author='maguowei',
+    author_email='imaguowei@gmail.com',
+    description='record github starred and change history',
+    py_modules=['starred'],
+    scripts=['starred.py'],
+    install_requires=[
+        'click==6.6',
+        'github3.py==1.0.0a4',
+    ],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+)
--- starred.py
+++ starred.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import click
 from github3 import GitHub
 
 
@@ -14,11 +17,17 @@ python starred.py > README.md
 
 '''
 
-if __name__ == '__main__':
-    gh = GitHub()
-    stars = gh.iter_starred('maguowei')
-    print(desc)
-    for s in stars:
-        #print('*', '[{}]({}) stars: {}, forks: {}, languages: {}, desc: {}'.format(s.full_name, s.html_url, s.stargazers, s.fork_count, s.language, s.description))
-        print('*', '[{}]({}) stars: {}, forks: {}, languages: {}'.format(s.full_name, s.html_url, s.stargazers, s.fork_count, s.language))
 
+@click.command()
+@click.option('--username', default='maguowei', help='GitHub username')
+def starred(username):
+    gh = GitHub()
+    stars = gh.starred_by(username)
+    click.echo(desc)
+    for s in stars:
+        keys = (s.full_name, s.html_url, s.stargazers_count, s.fork_count, s.language)
+        data = '* [{}]({}) stars: {}, forks: {}, languages: {}'.format(*keys)
+        click.echo(data)
+
+if __name__ == '__main__':
+    starred()
