570f884a4ef308a2b3f1d41071b479ec25a1d40f
maguowei <imaguowei@gmail.com>
2016-07-09 23:17:48 +0800

    add support python2.7

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name='starred',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/maguowei/starred',
     license='The MIT License (MIT)',
     author='maguowei',
     author_email='imaguowei@gmail.com',
-    description='GitHub starred',
+    description='''GitHub starred
+     make your own awesome lists page by GitHub star!
+
+     example:
+        starred --username maguowei --sort > README.md
+    ''',
     py_modules=['starred'],
     install_requires=[
         'click==6.6',
@@ -19,6 +24,7 @@ setup(
     ''',
     classifiers=(
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
--- starred.py
+++ starred.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
+import sys
 from collections import OrderedDict
 import click
 from github3 import GitHub
 
+PY2 = sys.version_info[0] == 2
 
 desc = '''# Starred
 
@@ -17,18 +19,26 @@ desc = '''# Starred
 @click.option('--username', default=lambda: os.environ.get('USER', ''), help='GitHub username')
 @click.option('--sort',  is_flag=True, help='sort by language')
 def starred(username, sort):
-    """
+    """GitHub starred
+
+    make your own awesome lists page by GitHub star!
+
+    example:
+        starred --username maguowei --sort > README.md
 
     """
     gh = GitHub()
     stars = gh.starred_by(username)
     click.echo(desc)
     repo_dict = {}
+
     for s in stars:
         language = s.language or 'Others'
-        description = s.description or ''
+        description = s.description.encode('utf-8') if PY2 else s.description
+
         if language not in repo_dict:
             repo_dict[language] = []
+
         repo_dict[language].append([s.name, s.html_url, description.strip()])
 
     if sort:
@@ -38,6 +48,7 @@ def starred(username, sort):
         data = '    - [{}](#{})'.format(language, language.lower())
         click.echo(data)
     click.echo('')
+
     for language in repo_dict:
         click.echo('## %s\n' % language)
         for repo in repo_dict[language]:
