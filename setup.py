a1dde2a8534d14a4a01cc9d7f854763ced85437b
maguowei <imaguowei@gmail.com>
2016-08-07 13:44:34 +0800

    tag version 1.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='starred',
-    version='1.3.0',
+    version='1.3.1',
     url='https://github.com/maguowei/starred',
     license='MIT',
     author='maguowei',
