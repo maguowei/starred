ba454b785de6d4e6dbcae2de125d82afe25bdfda
maguowei <imaguowei@gmail.com>
2018-06-19 20:31:55 +0800

    release v2.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='starred',
-    version='1.3.1',
+    version='2.0.0',
     url='https://github.com/maguowei/starred',
     license='MIT',
     author='maguowei',
