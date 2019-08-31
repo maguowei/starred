e0511cfae37645fc3fd85045253bfb6288e313c3
maguowei <imaguowei@gmail.com>
2019-08-31 15:18:39 +0800

    close #36 add python-requires

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     platforms='any',
+    python_requires='>=3.5',
     install_requires=[
         'click==7.0',
         'github3.py==1.3.0',
@@ -38,6 +39,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
